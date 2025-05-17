acf63403d8974ea4f2b4053243ff77e090ed18a7
oddelaney <oisindelaney02@outlook.com>
2025-05-17 21:33:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const map = L.map("map").setView([52.0907, 5.1214], 13);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+  L.marker([52.0907, 5.1214]).addTo(map).bindPopup("Utrecht – testmarker");
+});
